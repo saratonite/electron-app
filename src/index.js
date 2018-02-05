5008008f5154e761c90277646c67c6d9d90ea8a5
Sarath <sarath.hacks@gmail.com>
2018-02-05 22:09:26 +0530

    :car: Style updates

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-
+import './index.css';
 const App = () => (
-    <div className="app">
-        It Works!!!
+    <div className="app-container">
+        <h1>It Works!!!</h1>
     </div>
 )
 
